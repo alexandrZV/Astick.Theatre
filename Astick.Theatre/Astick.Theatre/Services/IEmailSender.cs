ab65eeca309ddb52a7d092fb44871028ec122024
Александр <zaycev_alexandr@bk.ru>
2017-12-20 23:44:53 +0300

    init

---
+++ Astick.Theatre/Astick.Theatre/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Astick.Theatre.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
